02daaf02bf9dbb42e670537f17e0efcd9af85289
Sierra Moore <sierra.v.moore@gmail.com>
2020-06-16 16:57:50 +0200

    basic image hover effect starter

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new hoverEffect ({
+    parent: document.querySelector('.container'),
+    intensity1: 1,
+    intensity2: 2,
+    speedIn: 1,
+    speedOut: 1,
+    image1: './img/img1.jpg',
+    image2: './img/img2.jpg',
+    displacementImage: './img/displacement/3.jpg'
+})
