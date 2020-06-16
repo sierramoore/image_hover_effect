d9db3ac8fb4b4c3ae50950102ef12c35deba59a2
Sierra Moore <sierra.v.moore@gmail.com>
2020-06-16 19:49:52 +0200

    added images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 new hoverEffect ({
-    parent: document.querySelector('.container'),
+    parent: document.querySelector('.container1'),
     intensity1: 1,
     intensity2: 2,
     speedIn: 1,
     speedOut: 1,
     image1: './img/img1.jpg',
     image2: './img/img2.jpg',
-    displacementImage: './img/displacement/3.jpg'
+    displacementImage: './img/displacement/8.jpg'
+})
+
+new hoverEffect ({
+    parent: document.querySelector('.container2'),
+    intensity1: 1,
+    intensity2: 2,
+    speedIn: 1,
+    speedOut: 1,
+    image1: './img/img3.jpg',
+    image2: './img/img4.jpg',
+    displacementImage: './img/displacement/8.jpg'
 })
